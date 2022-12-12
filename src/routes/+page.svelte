a2a018f5acee810ad6aabb72f47b7404f8ccafdc
Gabriel Do Couto Santos <gabrieldocoutosantos@Gabriels-MacBook-Pro.local>
2022-12-12 09:42:25 -0300

    start playing arount with tauri + svelte kit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let content = "";
+
+	function handleKeyPress(event: KeyboardEvent) {
+		content += event?.key;
+	}
+</script>
+
+<div>
+	<div on:keydown={handleKeyPress} tabindex="-1" class="container">
+		{content}
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 200px;
+		height: 200px;
+		background-color: red;
+	}
+</style>
